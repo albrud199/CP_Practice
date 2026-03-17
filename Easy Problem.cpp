945cdded18b617a7701e6d7b3efd18a0fb8d236e
_albin_rudro_ <145426165+albrud199@users.noreply.github.com>
2026-03-17 14:39:01 +0600

    Easy Problem.cpp

---
+++ Easy Problem.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t,n,a;
+    cin>>t;
+    
+    while(t--)
+    {
+        cin>>n;
+        cout<<n-1<<endl;
+    }
+    
+    return 0;
+}
